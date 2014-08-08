280ab292d76b0ff2390399644e92dd3f8e370984
apendua <apendua@gmail.com>
2014-08-08 21:05:39 +0200

    Build before running tests

--- test.js
+++ test.js
@@ -3,11 +3,9 @@
 var Mocha = require('mocha');
 var path = require('path');
 var fs = require('fs');
-//var gagarin = require('./gagarin');
-
-//gagarin.config({
-//  mongoPath: '~/.meteor/tools/latest/mongodb/bin/mongod'
-//});
+var buildAsPromise = require('./build');
+var colors = require('colors');
+var pathToApp = path.resolve('./tests/example');
 
 var mocha = new Mocha({
   reporter : 'spec',
@@ -18,9 +16,21 @@ fs.readdirSync(path.join(__dirname, 'tests', 'specs')).forEach(function (file) {
   mocha.addFile(path.join(__dirname, 'tests', 'specs', file));
 });
 
-mocha.run(function (failedCount) {
-  if (failedCount > 0) {
-    process.exit(1);
-  }
-  process.exit(0);
+process.stdout.write('\n  Building Your App [[['.blue + pathToApp.blue + ']]] /'.blue);
+var counter = 0;
+var handle = setInterval(function () {
+  process.stdout.write('\b' + '/-\\|'.charAt(counter++ % 4).blue);
+}, 100);
+
+buildAsPromise(pathToApp).then(function () {
+
+  clearInterval(handle);
+
+  mocha.run(function (failedCount) {
+    if (failedCount > 0) {
+      process.exit(1);
+    }
+    process.exit(0);
+  });
+
 });
