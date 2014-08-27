7d1228a0dd3a2a44949a6c594bdc8a0a7b88664f
Hubert OG <hubert@orlikarnia.com>
2014-08-27 18:18:05 +0200

    Update fix

--- package.js
+++ package.js
@@ -2,13 +2,13 @@
 Package.describe({
   summary:  "Gagarin bindings for Meteor",
   name:     "anti:gagarin",
-  version:  "0.1.1",
+  version:  "0.1.2",
   git:      "https://github.com/anticoders/gagarin.git",
 });
 
 Package.on_use(function (api) {
 
-  api.use('apendua:altimeter@0.0.2', 'server');
+  api.use('mrt:altimeter@0.0.2', 'server');
 
   api.add_files([
     'meteor_hooks.js'
