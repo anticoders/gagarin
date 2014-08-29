8921cdf65d44fc31df6b073e0f2e8aa337e4f36d
apendua <apendua@gmail.com>
2014-08-29 16:02:24 +0200

    Discover development bundle based on the Meteor version

--- tests/specs/benchmark.js
+++ tests/specs/benchmark.js
@@ -26,7 +26,7 @@ describe('Benchmark test suite', function () {
   it('should be able to find the release config', function () {
     var config = tools.getReleaseConfig(pathToApp);
     expect(config.tools).to.be.ok;
-    expect(config.packages).not.to.be.empty;
+    expect(config.tools).not.to.be.equal('latest');
   });
 
   it('should be able to build app', function () {
