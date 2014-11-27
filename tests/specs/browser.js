b8cc0e484eef62ac4684a696f9da6ac06471d0ef
Tomasz Lenarcik <apendua@gmail.com>
2014-11-27 21:49:25 +0100

    Use webdriver for browser tests

--- tests/specs/browser.js
+++ tests/specs/browser.js
@@ -1,16 +1,13 @@
 var Promise = require('es6-promise').Promise;
 var Gagarin = require('../../gagarin');
-var PhantomAsPromise = require('phantom-as-promise').PhantomAsPromise;
 var path = require('path');
 var expect = require('chai').expect;
+var wd = require('wd');
 
 describe('Tests with phantomjs browser', function () {
 
-  var page = null;
-  
-  var phantom = new PhantomAsPromise({
-    phantomPath: require('phantomjs').path
-  }, require('phantom-as-promise').meteor_helpers);
+  var browser1 = wd.promiseChainRemote('http://localhost:9515');
+  var browser2 = wd.promiseChainRemote('http://localhost:9515');
 
   var gagarin = new Gagarin({
     pathToApp: path.resolve('./tests/example')
@@ -23,13 +20,15 @@ describe('Tests with phantomjs browser', function () {
   });
   
   before(function () {
-    page = phantom.page();
-    return page.open(gagarin.location);
+    return browser1
+      .init()
+      .get(gagarin.location);
   });
 
   after(function () {
     return Promise.all([
-      phantom.exit(),
+      browser1.quit(),
+      browser2.quit(),
       gagarin.exit(),
     ]);
   });
@@ -39,57 +38,54 @@ describe('Tests with phantomjs browser', function () {
   });
 
   it('eval should work in browser', function () {
-    return page.eval(function () {
-      return Meteor.release;
-    })
-    .then(function (value) {
-      expect(value).not.to.be.empty;
-    });
+    return browser1.execute("return Meteor.release;")
+      .then(function (value) {
+        expect(value).not.to.be.empty;
+      });
   });
       
   var id = Math.floor(1000 * Math.random()).toString();
 
   describe('Database insertions', function () {
     before(function () {
-      return page.promise(function (resolve, reject, id) {
-        Items.insert({_id: id}, either(reject).or(resolve));
-      }, id)
-      .then(function (value) {
-        expect(value).to.equal(id);
-      });
+      return browser1
+        .setAsyncScriptTimeout(100)
+        .executeAsync(
+          "var cb = arguments[arguments.length-1];\n" +
+          "Items.insert({_id: " + JSON.stringify(id) + "}, function (err, res) { cb(res) });"
+        )
+        .then(function (value) {
+          expect(value).to.equal(id);
+        });
     });
 
     it('db insert should work in browser', function () {
-      return page.eval(function (id) {
-        return Items.findOne({_id: id});
-      }, id)
-      .then(function (value) {
-        expect(value).not.to.be.empty;
-        expect(value._id).to.equal(id);
-      });
+      return browser1.execute(
+          "return Items.findOne({_id: " + JSON.stringify(id) + "});"
+        )
+        .then(function (value) {
+          expect(value).not.to.be.empty;
+          expect(value._id).to.equal(id);
+        });
     });
     
     it('the same element should be present on server', function () {
       return gagarin.eval(function (id) {
-        // TODO: wait?
-        return Items.findOne({_id: id});
-      }, id)
-      .then(function (value) {
-        expect(value).not.to.be.empty;
-        expect(value._id).to.equal(id);
-      });
+          // TODO: wait?
+          return Items.findOne({_id: id});
+        }, id)
+        .then(function (value) {
+          expect(value).not.to.be.empty;
+          expect(value._id).to.equal(id);
+        });
     });
 
   });
 
   describe('Restarting server', function () {
 
-    var anotherPage = null;
-
     before(function () {
-      anotherPage = phantom.page();
-      return anotherPage.open(gagarin.location)
-        .waitForMeteor();
+      return browser2.init().setAsyncScriptTimeout(10000).get(gagarin.location);
     });
 
     before(function () {
@@ -98,29 +94,29 @@ describe('Tests with phantomjs browser', function () {
 
     it ('should be all right', function () {
       return gagarin.eval(function ()  {
-        return Meteor.release;
-      })
-      .then(function (release) {
-        expect(release).to.be.ok;
-      });
+          return Meteor.release;
+        })
+        .then(function (release) {
+          expect(release).to.be.ok;
+        });
     });
 
     it('should recognize that the server was restarted', function () {
-      return anotherPage.wait(5000, 'until reset value changes', function () {
-        return reset;
-      })
-      .then(function (numberOfResets) {
-        expect(numberOfResets).to.equal(1);
-      });
+      return browser2
+        .waitForConditionInBrowser("reset > 0", 5000)
+        .execute("return reset;")
+        .then(function (numberOfResets) {
+          expect(numberOfResets).to.equal(1);
+        });
     });
 
     it ('another restart shoud work as well', function () {
       return gagarin.restart().eval(function ()  {
-        return Meteor.release;
-      })
-      .then(function (release) {
-        expect(release).to.be.ok;
-      });
+          return Meteor.release;
+        })
+        .then(function (release) {
+          expect(release).to.be.ok;
+        });
     });
 
   });
