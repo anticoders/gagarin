805b1168384cec86237431950b2fa1aa6ad498e2
Tomasz Lenarcik <apendua@gmail.com>
2014-12-09 20:20:25 +0100

    Bumped package version

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   summary:  "Gagarin, a Meteor testing framework",
   name:     "anti:gagarin",
-  version:  "0.3.0-pre1",
+  version:  "0.3.0-pre2",
   git:      "https://github.com/anticoders/gagarin.git",
 });
 
