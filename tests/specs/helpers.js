d6c29892dc8571bb39c1cd41ff1c405b4dfe9085
apendua <apendua@gmail.com>
2014-12-22 21:11:27 +0100

    Make sure custom helpers and chaining works

--- tests/specs/helpers.js
+++ tests/specs/helpers.js
@@ -1,18 +1,44 @@
+var Promise = require('es6-promise').Promise;
+
 describe('Helpers', function () {
 
-  var server = meteor();
-  var client = browser(server.location);
+  describe('Built in helpers', function () {
 
-  it('should be able to use sendKeys', function () {
-    return client
-      .sendKeys('input[type=text]', 'abc')
-      .expectValueToEqual('input[type=text]', 'abc');
+    var server = meteor();
+    var client = browser(server.location);
+
+    it('should be able to use sendKeys', function () {
+      return client
+        .sendKeys('input[type=text]', 'abc')
+        .expectValueToEqual('input[type=text]', 'abc');
+    });
+
+    it('should be able to use click', function () {
+      return client
+        .click('input[type=button]')
+        .expectTextToContain('p', '1');
+    });
+
   });
 
-  it('should be able to use click', function () {
-    return client
-      .click('input[type=button]')
-      .expectTextToContain('p', '1');
+  describe('Custom user-defined helpers', function () {
+
+    var server = meteor({
+      helpers: {
+        sleepForOneSecond: function () {
+          return this.then(function () {
+            return new Promise(function (resolve) {
+              setTimeout(resolve, 1000);
+            });
+          });
+        },
+      },
+    });
+
+    it('should be able to use a custom helper', function () {
+      return server.sleepForOneSecond();
+    });
+
   });
 
 });
