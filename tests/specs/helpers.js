f7e9ea22550e6196c33891f45d209f32baf0ea23
davluzius <davluzius@gmail.com>
2015-01-31 19:02:39 -0800

    add accounts helper tests

--- tests/specs/helpers.js
+++ tests/specs/helpers.js
@@ -2,7 +2,7 @@ var Promise = require('es6-promise').Promise;
 
 describe('Helpers', function () {
 
-  describe('Built in helpers', function () {
+  describe('Built in DOM helpers', function () {
 
     var server = meteor();
     var client = browser(server);
@@ -111,6 +111,66 @@ describe('Helpers', function () {
           expect(res).to.be.true;
         });
     });
+
+  });
+  
+  describe('Built in Accounts helpers', function () {
+    
+    var server = meteor();
+    var client = browser(server);
+
+    before(function () {
+      return server.execute(function () {
+        Accounts.createUser({email: 'existingUser@example.com',password: 'password'});
+      })    
+    });
+
+    it('signUp should create a new user with email option', function () {
+      return client
+        .signUp({email: 'test@example.com',password: 'password'})
+        .execute(function () {
+          return Meteor.users.findOne({'emails.address': 'test@example.com'});
+        })
+        .then(function(res) {
+          var email = res.emails[0].address;
+          expect(email).to.eql('test@example.com');
+        });
+    });
+
+    it('signUp should create a new user with username option', function () {
+      return client
+        .signUp({username: 'testName',password: 'password'})
+        .execute(function () {
+          return Meteor.users.findOne({username: 'testName'});
+        })
+        .then(function(res) {
+          expect(res.username).to.eql('testName');
+        });
+    });
+
+    it('login should login existing user', function () {
+      return client
+        .login('existingUser@example.com','password')
+        .execute(function () {
+          return Meteor.user();
+        })
+        .then(function(res) {
+          var email = res.emails[0].address;
+          expect(email).to.eql('existingUser@example.com');
+        });
+    });
+
+    it('logout should logout logged in user', function () {
+      return client
+        .logout()
+        .execute(function () {
+          return Meteor.user();
+        })
+        .then(function(res) {
+          expect(res).to.be.null;
+        });
+    });
+
 
   });
 
