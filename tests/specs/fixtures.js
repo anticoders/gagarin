014d1426833411ea4319b72f96d2a60a8b7eb274
Tomasz Lenarcik <apendua@gmail.com>
2015-02-18 09:27:04 +0100

    Basic code fixtures now works fine

---
+++ tests/specs/fixtures.js
@@ -0,0 +1,112 @@
+
+describe('Fixtures', function () {
+
+  describe('Basic code injection', function () {
+
+    var server = meteor();
+
+    server.addJavaScript('client', function () {
+
+    });
+
+    server.addJavaScript('server', function () {
+      
+    });
+
+  });
+
+  describe.only('Unit tests with mocha', function () {
+
+    var server = meteor({ mocha: true });
+    var client = browser(server);
+
+    server.addJavaScript('server', function () {
+
+      describe('Super duper server test suite', function () {
+
+        it('should just work', function () {
+
+        });
+
+        it('should throw an error', function () {
+          throw new Error('this is a simulated error');
+        });
+
+      });    
+
+    });
+
+    server.addJavaScript('client', function () {
+
+      describe('Super duper client test suite', function () {
+
+        it('should just work', function () {
+
+        });
+
+        it('should throw an error', function () {
+          throw new Error('this is a simulated error');
+        });
+
+      });    
+
+    });
+
+    it('should receive unit tests results from server', function () {
+      return server.runMocha();
+    });
+
+    it('should receive unit tests results from client', function () {
+      return client.runMocha();
+    });
+
+  });
+
+  describe('Unit tests for package', function () {
+
+    var server = meteor({ mocha: true });
+    var client = browser(server);
+
+    server.addJavaScript('server', { toPackage: 'my-package' }, function () {
+
+      describe('Super duper server test suite', function () {
+
+        it('should just work', function () {
+
+        });
+
+        it('should throw an error', function () {
+          throw new Error('this is a simulated error');
+        });
+
+      });    
+
+    });
+
+    server.addJavaScript('client', { toPackage: 'my-package' }, function () {
+
+      describe('Super duper client test suite', function () {
+
+        it('should just work', function () {
+
+        });
+
+        it('should throw an error', function () {
+          throw new Error('this is a simulated error');
+        });
+
+      });    
+
+    });
+
+    it('should receive unit tests results from server', function () {
+      return server.runMocha();
+    });
+
+    it('should receive unit tests results from client', function () {
+      return client.runMocha();
+    });
+
+  });
+
+});
