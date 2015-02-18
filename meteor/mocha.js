dd604caa8e0d9e003c3033215857dcfb99b879f8
Tomasz Lenarcik <apendua@gmail.com>
2015-02-18 23:06:06 +0100

    For feedback use WebSocket-based solution

--- meteor/mocha.js
+++ meteor/mocha.js
@@ -1,14 +1,31 @@
 
-var reports = new Mongo.Collection("gagarin_reports");
+var socket = null;
 
-if (Meteor.isServer) {
-  Meteor.publish('/gagarin/reports', function () {
-    return reports.find({});
+Gagarin.feedback = function feedback (what, data) {
+
+  var feedback = _.extend({ what: what }, data);
+
+  socket && socket.write(JSON.stringify(feedback) + '\n');
+};
+
+Gagarin.setFeedbackUrl = function setReportsUrl (url, done) {
+
+  var WebSocket = Npm.require('faye-websocket');
+
+  done = _.once(done);
+
+  socket = new WebSocket.Client(url, [], { ping: 1 });
+
+  socket.on('open', function () {
+    done();
   });
+
+  socket.on('error', function (err) {
+    done(err);
+  });
+
+  socket.on('close', function () {
+    done(new Error('socket closed prematurely'));
+  });
+
 }
-
-var when = 0;
-
-Gagarin.report = Meteor.bindEnvironment(function report (what, data) {
-  reports.insert(_.extend({ what: what, when: when++ }, data));
-});
