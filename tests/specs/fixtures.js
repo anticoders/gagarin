840c85b140fa94f36a0a7cf8665edb139e6a9bee
Tomasz Lenarcik <apendua@gmail.com>
2015-02-19 17:46:42 +0100

    Mocha can now run on client too

--- tests/specs/fixtures.js
+++ tests/specs/fixtures.js
@@ -20,49 +20,15 @@ describe('Fixtures', function () {
     var server = meteor({ mocha: true });
     var client = browser(server);
 
-    server.addJavaScript('server', function () {
-
-      describe('Super duper server test suite', function () {
-
-        var i = 0;
-
-        for (i = 0; i < 30; i++) {
-          (function (i) {
-            it('should just work ' + i, function (done) { setTimeout(done, 2 * i); });
-            it('should throw an error', function (done) {
-              setTimeout(function () {
-                done(new Error('this is a simulated error'));
-              }, 2 * i);
-            });
-          })(i);
-        }
-
-      });    
-
-    });
-
-    server.addJavaScript('client', function () {
-
-      describe('Super duper client test suite', function () {
-
-        it('should just work', function () {
-
-        });
-
-        it('should throw an error', function () {
-          throw new Error('this is a simulated error');
-        });
-
-      });    
-
-    });
+    server.addJavaScript('server', superDuperTestSuite);
+    server.addJavaScript('client', superDuperTestSuite);
 
     it('should receive unit tests results from server', function () {
       return server.mocha().expectError(/30.*\n.*this is a simulated error/);
     });
 
     it('should receive unit tests results from client', function () {
-      return client.mocha();
+      return client.mocha().expectError(/30.*\n.*this is a simulated error/);
     });
 
   });
@@ -115,3 +81,20 @@ describe('Fixtures', function () {
   });
 
 });
+
+
+function superDuperTestSuite() {
+  describe('Super duper test suite', function () {
+    var i = 0;
+    for (i = 0; i < 30; i++) {
+      (function (i) {
+        it('should just work ' + i, function (done) { setTimeout(done, 2 * i); });
+        it('should throw an error', function (done) {
+          setTimeout(function () {
+            done(new Error('this is a simulated error'));
+          }, 2 * i);
+        });
+      })(i);
+    }
+  });      
+}
