fc7eba2f311e194fc916a487f92d96545b75bc9c
apendua <apendua@gmail.com>
2015-05-03 08:55:41 +0200

    Changed version to pre-release

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   summary:  "Gagarin, a Meteor testing framework based on mocha",
   name:     "anti:gagarin",
-  version:  "0.4.9",
+  version:  "0.4.10-pre1",
   git:      "https://github.com/anticoders/gagarin.git",
 });
 
