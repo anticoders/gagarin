6e5228ac35f0d13980a242f0a35fcade0891cdcf
apendua <apendua@gmail.com>
2015-12-14 21:17:46 +0100

    Looks like we don't even need a build plugin

--- packages/gagarin-trajectory/package.js
+++ packages/gagarin-trajectory/package.js
@@ -1,12 +1,54 @@
 
+var fs   = Npm.require('fs');
+var path = Npm.require('path');
+
 Package.describe({
-  summary : 'Collects data required to run Gagarin tests',
-  name    : 'gagarin:trajectory',
-  version : '0.4.11',
-  git     : 'https://github.com/anticoders/gagarin.git',
+  summary   : 'Collects data required to run Gagarin tests',
+  name      : 'gagarin:trajectory',
+  version   : '0.4.11',
+  git       : 'https://github.com/anticoders/gagarin.git',
+  debugOnly : true,
 });
 
-Package.registerBuildPlugin({
-  name    : 'createGagarinTrajectoryFile',
-  sources : [ 'plugin.js' ],
+Package.onUse(function () {
+
+  var pathToGagarin    = path.resolve('.gagarin');
+  var pathToGitIgnore  = path.join(pathToGagarin, '.gitignore');
+  var pathToLocal      = path.join(pathToGagarin, 'local');
+  var pathToTrajectory = path.join(pathToLocal, 'trajectory.json');
+
+  fs.mkdir(pathToGagarin, function (e) {
+    'use strict';
+    
+    if (e && e.code !== 'EEXIST') {
+      console.error(e.stack);
+      return;
+    }
+
+    fs.mkdir(pathToLocal, function (e) {
+      
+      if (e && e.code !== 'EEXIST') {
+        console.error(e.stack);
+        return;
+      }
+
+      fs.stat(pathToGitIgnore, function (e) {
+        if (e && e.code !== 'ENOENT') {
+          console.error(e.stack);
+          return;
+        }
+        fs.writeFile(pathToGitIgnore, 'local\n');
+      });
+
+      fs.writeFile(pathToTrajectory, JSON.stringify({
+        pathToNode: process.argv[0]
+      }, null, 2));
+
+    });
+
+  });
+
 });
+
+
+
