e4ea61ad336f77812dfd8697259d7b4c60dd927a
apendua <apendua@gmail.com>
2015-12-31 16:21:46 +0100

    Playing around with mocha ui

--- packages/gagarin-mocha/src/ui.js
+++ packages/gagarin-mocha/src/ui.js
@@ -10,25 +10,43 @@ export default function gagarin (suite) {
   // build on top of the standard bdd interface
   Mocha.interfaces.bdd.apply(this, arguments);
 
-  suite.on('pre-require', function (context) {
+  suite.beforeAll(function () {
 
-    var before = context.before;
-    var after  = context.after;
+  });
 
-    context.meteor = function (options, onStart) {
+  suite.afterAll(function () {
 
+  });
+
+  suite.on('pre-require', function (context, file, mocha) {
+
+    let before = context.before;
+    let after  = context.after;
+
+    context.meteor = function (options) {
+      
     };
 
-    context.browser = function (location, options, initialize) {
-
+    context.browser = function (options) {
+      
     };
 
     context.ddp = function (options) {
-
+      
     };
 
     context.mongo = function (options) {
+      
+    };
 
+    context.gagarin = {
+      launch () {
+        setTimeout(function () {
+          suite.run(function (...args) {
+            console.log('SUITE READY', args);
+          });
+        }, 2000);
+      }
     };
 
   });
