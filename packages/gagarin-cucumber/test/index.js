33d0f1a152ff1f9bd197f39a0edc4956076ea4c6
apendua <apendua@gmail.com>
2016-01-01 22:15:27 +0100

    Removing unnecessary packages

--- packages/gagarin-cucumber/test/index.js
+++
@@ -1,9 +0,0 @@
-// babel compiler & runtime
-require("babel-register");
-
-// we should be able to get rid of polyfill as soon as
-// https://phabricator.babeljs.io/T6676 is fixed
-require("babel-polyfill");
-
-// actual tests
-require("./test.js");
--- packages/gagarin-mocha/src/index.js
+++
@@ -1,2 +0,0 @@
-import ui from './ui';
-export {ui};
